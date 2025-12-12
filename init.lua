521deb15a4c316eca0867b30fb6ac123e92a5301
TomJDonahue <tdonahue630@gmail.com>
2025-12-12 11:46:32 -0500

    Added diagnostic floating

--- init.lua
+++ init.lua
@@ -181,7 +181,9 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
+--MY FOURTH KEYMAP. Diagnostic navigation and floating diagnostics
+vim.keymap.set('n', '<leader>dd', vim.diagnostic.open_float, { desc = 'Set Open Float' })
+vim.diagnostic.config { jump = { float = true } }
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
