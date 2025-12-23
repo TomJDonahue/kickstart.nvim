3e0bb54a3b10ae0f7ae4336f37aab709e31f7a6a
TomJDonahue <tdonahue630@gmail.com>
2025-12-23 07:11:55 -0500

    fixed a tab conflict in tabout.nvim

--- init.lua
+++ init.lua
@@ -300,6 +300,7 @@ require('lazy').setup({
     config = function()
       require('tabout').setup {
         tabkey = '<Tab>', -- key to trigger tabout, set to an empty string to disable
+        -- tabkey = '',
         backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
         act_as_tab = true, -- shift content if tab out is not possible
         act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
@@ -315,7 +316,7 @@ require('lazy').setup({
           { open = '[', close = ']' },
           { open = '{', close = '}' },
         },
-        ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+        ignore_beginning = false, --[[ if true, if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
         exclude = {}, -- tabout will ignore these filetypes
       }
     end,
