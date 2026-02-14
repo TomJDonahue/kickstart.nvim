2aafe483bd824b40e63d0d57611c16e060e649fe
TomJDonahue <tdonahue630@gmail.com>
2026-02-14 08:51:26 -0500

    added treesitter

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -281,7 +282,23 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sn', function() builtin.find_files { cwd = vim.fn.stdpath 'config' } end, { desc = '[S]earch [N]eovim files' })
     end,
   },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
+    build = ':TSUpdate',
+    config = function()
+      local ts = require 'nvim-treesitter'
+      local parsers = { 'odin', 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
 
+      for _, parser in ipairs(parsers) do
+        pcall(ts.install, parser)
+      end
+
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function() pcall(vim.treesitter.start) end,
+      })
+    end,
+  },
   {
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
